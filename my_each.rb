b33fb35d889029638324eed4a62cd0f1c0b94203
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-04-04 01:09:36 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arr)
+  counter = 0 
+  while counter < arr.length 
+  yield arr[counter]
+  counter += 1 
+  end
+  arr 
+end
+
+new = []
+my_each([1, 2, 3, 4]) do |num| 
+  new << num + 10 
 end
